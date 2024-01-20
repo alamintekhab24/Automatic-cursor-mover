24f624fe10c049b97082405e72ee904b82978b01
Intekhab Alam <157228194+alamintekhab24@users.noreply.github.com>
2024-01-20 21:07:27 +0530

    Add files via upload

---
+++ mouse.py
@@ -0,0 +1,32 @@
+"""import pyautogui as pag
+import random
+import time
+
+while True:
+    x = random.randint(600,700)
+    y = random.randint(200,600)
+    pag.moveTo(x,y,0.5)
+    time.sleep(10)
+_________________________________________________________"""
+
+import pyautogui
+import time
+import sys
+from datetime import datetime
+pyautogui.FAILSAFE = False
+numMin = None
+if ((len(sys.argv)<2) or sys.argv[1].isalpha() or int(sys.argv[1])<1):
+    numMin = 3
+else:
+    numMin = int(sys.argv[1])
+while(True):
+    x=0
+    while(x<numMin):
+        time.sleep(60)
+        x+=1
+    for i in range(0,200):
+        pyautogui.moveTo(0,i*4)
+    pyautogui.moveTo(1,1)
+    for i in range(0,3):
+        pyautogui.press("shift")
+    print("Movement made at {}".format(datetime.now().time()))
